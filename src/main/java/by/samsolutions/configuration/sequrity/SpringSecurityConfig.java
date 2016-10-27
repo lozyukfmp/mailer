93e39af1ae52a17470513a6c0a26c3e43f5142f1
Lozyuk Artem <lozyukartem@gmail.com>
2016-10-27 03:05:03 +0300

    add password encoder

--- src/main/java/by/samsolutions/configuration/sequrity/SpringSecurityConfig.java
+++ src/main/java/by/samsolutions/configuration/sequrity/SpringSecurityConfig.java
@@ -1,14 +1,16 @@
 package by.samsolutions.configuration.sequrity;
 
-import by.samsolutions.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -23,7 +25,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -41,5 +44,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                     .exceptionHandling().accessDeniedPage("/accessDenied")
                 .and()
                     .csrf();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
     }
 }
--- src/main/java/by/samsolutions/service/impl/UserServiceImpl.java
+++ src/main/java/by/samsolutions/service/impl/UserServiceImpl.java
@@ -3,10 +3,11 @@ package by.samsolutions.service.impl;
 import by.samsolutions.dao.UserDao;
 import by.samsolutions.dto.UserDto;
 import by.samsolutions.entity.user.User;
-import by.samsolutions.entity.user.UserInfo;
 import by.samsolutions.entity.user.UserRole;
 import by.samsolutions.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     @Transactional
@@ -42,7 +46,7 @@ public class UserServiceImpl implements UserService {
         userRoleSet.add(userRole);
 
         user.setUsername(accountDto.getUsername());
-        user.setPassword(accountDto.getPassword());
+        user.setPassword(passwordEncoder.encode(accountDto.getPassword()));
         user.setUserRole(userRoleSet);
         user.setEnabled(true);
 
